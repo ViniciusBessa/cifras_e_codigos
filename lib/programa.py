c4d5a11da2ef7005e52d084fbf32f92e2afeb99e
Vinicius <vinicius.b.silva3@gmail.com>
2021-09-21 17:38:28 -0300

    Mudança do dicionário para uma lista

--- lib/programa.py
+++ lib/programa.py
@@ -3,30 +3,31 @@ from time import sleep
 from .codificadores import *
 from .decodificadores import *
 
-# Dicionário para acessar o nome da criptografia e suas funções
-opcoes_cripto = {
-            1: ['Cifra de César', cod_cesar, decod_cesar],
-            2: ['Cifra de Vigenère', cod_vigenere, decod_vigenere],
-            3: ['One-time pad (Cifra de uso único)', cod_onetimepad, decod_onetimepad],
-            4: ['Código morse', cod_morse, decod_morse],
-            5: ['Tap code (Código da batida)', cod_tapcode, decod_tapcode],
-            6: ['Autokey cipher (Cifra de autochave)', cod_autokey, decod_autokey],
-            7: ['Cifra niilista', cod_niilista, decod_niilista]
-            }
+# Lista contendo o nome da criptografia e suas funções
+opcoes_cripto = [
+    ['Cifra de César', cod_cesar, decod_cesar],
+    ['Cifra de Vigenère', cod_vigenere, decod_vigenere],
+    ['One-time pad (Cifra de uso único)', cod_onetimepad, decod_onetimepad],
+    ['Código morse', cod_morse, decod_morse],
+    ['Tap code (Código da batida)', cod_tapcode, decod_tapcode],
+    ['Autokey cipher (Cifra de autochave)', cod_autokey, decod_autokey],
+    ['Cifra niilista', cod_niilista, decod_niilista]
+]
 
 
 def programa() -> None:
     """Função que recebe qual das opções de criptografias o usuário quer executar"""
     print('\n' * 15)
     print('Menu principal\n')
-    for opcao, valores in opcoes_cripto.items():
-        print(str(opcao) + ' - ' + valores[0])
+
+    for opcao, valores in enumerate(opcoes_cripto):
+        print(str(opcao + 1) + ' - ' + valores[0])
     print(str(len(opcoes_cripto) + 1) + ' - Sair do programa')
     print()
 
     try:
         opcao_escolhida: int = int(input('Digite umas da opções: '))
-        efetuar_opcao(opcao_escolhida)
+        efetuar_opcao(opcao_escolhida - 1)
 
     except ValueError:
         print('Opção inválida.')
@@ -38,6 +39,7 @@ def codificar_ou_decodificar() -> int:
     """Função para saber se o usuário deseja codificar ou decodificar"""
     print('Codificar ou decodificar\n')
     print('1 - Codificar\n2 - Decodificar')
+
     try:
         escolha: int = int(input('Digite uma das opções: '))
         print('\n' * 15)
@@ -45,6 +47,7 @@ def codificar_ou_decodificar() -> int:
             return escolha
         else:
             print('Opção inválida')
+
     except ValueError:
         print('\n' * 15)
         print('Opção inválida')
@@ -52,14 +55,14 @@ def codificar_ou_decodificar() -> int:
 
 def efetuar_opcao(escolha_cripto) -> None:
     """Função que verifica se o usuário escolheu uma opção válida, e se sim, ela é executada"""
-    escolha_operacao = ''
+    escolha_operacao = 0
 
-    if escolha_cripto in opcoes_cripto.keys():
+    if 0 <= escolha_cripto < len(opcoes_cripto):
         while escolha_operacao != 1 and escolha_operacao != 2:
             escolha_operacao = codificar_ou_decodificar()
         print(opcoes_cripto[escolha_cripto][escolha_operacao]())
 
-    elif escolha_cripto == len(opcoes_cripto) + 1:
+    elif escolha_cripto == len(opcoes_cripto):
         print('Programa finalizado.')
         exit()
 
